ba68bb9eeb9d3ce7e1caa584042e412aa54268fa
Blen Wondimagegn <mekuriablen@gmail.com>
2020-09-17 06:10:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,10 +48,10 @@ class Student
  
   end
  
-  # def self.create(name, album)
-  #   song = Song.new(name, album)
-  #   song.save
-  #   song
-  # end
+  def self.create(name, album)
+    song = Song.new(name, album)
+    song.save
+    song
+  end
   
 end
